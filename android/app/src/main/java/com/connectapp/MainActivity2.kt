e5354b7ecf98d733eab19923639f59f82590ef09
Inno stacks <innostatcks@gmail.com>
2024-05-15 16:19:16 +0530

    added multiple activity

--- android/app/src/main/java/com/connectapp/MainActivity2.kt
+++ android/app/src/main/java/com/connectapp/MainActivity2.kt
@@ -1,11 +1,27 @@
 package com.connectapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.connectapp.location.LocationCaptureActivity
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+       var btn = findViewById<Button>(R.id.button)
+        btn.setOnClickListener {
+            startLocation()
+        }
+
+        //startLocation()
     }
+
+    fun startLocation(){
+
+        startActivity(Intent(this,LocationCaptureActivity::class.java))
+    }
+
 }
---
+++ android/app/src/main/java/com/connectapp/location/LocationCaptureActivity.kt
@@ -0,0 +1,12 @@
+package com.connectapp.location
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.connectapp.R
+
+class LocationCaptureActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_location_capture)
+    }
+}
